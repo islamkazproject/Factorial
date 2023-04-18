6e70eeb6620fcb8886e2d1bfd09c2cdb48edba45
islam <islamkazproject@gmail.com>
2023-04-18 18:48:20 +0300

    Add first task.

---
+++ 1.py
@@ -0,0 +1,9 @@
+
+day = 18
+month = "апреля"
+temperature = -15
+
+print(f'Сегодня {day} {month}. На улице {temperature} градусов.')
+
+if temperature < 0:
+    print("Холодно, лучше остаться дома.")
